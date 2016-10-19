ed290a4bf1e2686d26baca7494bfdd6496d9ee51
Jan Plas <janplas@iMac.local>
2016-10-19 12:39:34 +0200

    Profile message update

--- vendor/crocodicstudio/crudbooster/src/controllers/UsersController.php
+++ vendor/crocodicstudio/crudbooster/src/controllers/UsersController.php
@@ -24,23 +24,23 @@ class UsersController extends CBController {
 	
 
 		$this->col = array();
-		$this->col[] = array("label"=>"Name","name"=>"name");
+		$this->col[] = array("label"=>"Naam","name"=>"name");
 		$this->col[] = array("label"=>"Email","name"=>"email");
-		$this->col[] = array("label"=>"Privilege","name"=>"id_cms_privileges","join"=>"cms_privileges,name");
-		$this->col[] = array("label"=>"Photo","name"=>"photo","image"=>1);
+		$this->col[] = array("label"=>"Rechten","name"=>"id_cms_privileges","join"=>"cms_privileges,name");
+		$this->col[] = array("label"=>"Afbelding","name"=>"photo","image"=>1);
 		$this->col[] = array("label"=>"Company","name"=>"id_cms_companies","join"=>'cms_companies,name');
 
 		$this->form = array(); 
 		$this->form[] = array("label"=>"Primary Data","type"=>"header");
 		$this->form[] = array("label"=>"Name","name"=>"name",'required'=>true,'validation'=>'required|alpha_spaces|min:3');
 		$this->form[] = array("label"=>"Email","name"=>"email",'required'=>true,'type'=>'email','validation'=>'required|email|unique:cms_users,email,'.get_row_id());		
-		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload_standard",'upload_file'=>false,"help"=>"Resolution recomended is 200x200px",'required'=>true,'validation'=>'required|image|max:1000');								
+		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload_standard",'upload_file'=>false,"help"=>"Aanbevolen formaat is 200x200px",'required'=>true,'validation'=>'required|image|max:1000');								
 		if(get_method() == 'getEdit' || get_method() == 'getAdd' || get_method() == 'postAddSave' || get_method() == 'postEditSave') {
 			$this->form[] = array("label"=>"Company","name"=>"id_cms_companies","type"=>"select","datatable"=>"cms_companies,name",'required'=>true);				
 			$this->form[] = array("label"=>"Privilege","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name",'required'=>true);		
 		}
-		$this->form[] = array("label"=>"Change the Password","type"=>"header");
-		$this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
+		$this->form[] = array("label"=>"Verander het wachtwoord","type"=>"header");
+		$this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Laat het wachtwoord leeg als je deze niet wilt aanpassen");
 
 		if(get_method() == 'getProfile') {
 			$this->button_addmore     = FALSE;
@@ -58,7 +58,7 @@ class UsersController extends CBController {
 	public function getProfile() {
 		$id = get_my_id();
 		Session::put('current_row_id',$id);
-		$data['page_title'] = "My Profile";
+		$data['page_title'] = "Mijn Profiel";
 		$data['row'] = DB::table($this->table)->where($this->primary_key,$id)->first();
 		$data['return_url'] = Request::url();
 		return view('crudbooster::default.form',$data);
